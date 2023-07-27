57cfa1da2de4583ebf6b62b043709c9688b91f6c
sayames <96351085+sayames@users.noreply.github.com>
2023-07-27 15:35:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const foregroundLayer = document.querySelector('.foreground-layer');
-const cliffLayer = document.querySelector('.cliff-layer');
 const backgroundLayer = document.querySelector('.background-layer');
+const cliffLayer = document.querySelector('.cliff-layer');
+const foregroundLayer = document.querySelector('.foreground-layer');
 
 window.addEventListener('scroll', function() {
     const scrollY = window.scrollY;
 
     // Adjust the translateY position based on scroll speed
-    foregroundLayer.style.transform = `translateY(-${scrollY * 0.5}px)`; // Adjust the scroll speed factor as needed
-    cliffLayer.style.transform = `translateY(-${scrollY * 1}px)`;
     backgroundLayer.style.transform = `translateY(-${scrollY * 0.2}px)`; // Adjust the scroll speed factor as needed
+    cliffLayer.style.transform = `translateY(-${scrollY * 0.5}px)`; // Adjust the scroll speed factor as needed
+    foregroundLayer.style.transform = `translateY(-${scrollY * 1}px)`; // Adjust the scroll speed factor as needed
 });
