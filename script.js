5ad32047ebedd5aebfeed048e9ec52e7d816110a
sayames <96351085+sayames@users.noreply.github.com>
2023-07-27 15:32:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const foregroundLayer = document.querySelector('.foreground-layer');
+const cliffLayer = document.querySelector('.cliff-layer');
+const backgroundLayer = document.querySelector('.background-layer');
+
 window.addEventListener('scroll', function() {
     const scrollY = window.scrollY;
-    const layers = document.querySelectorAll('.parallax-layer');
 
-    layers.forEach(layer => {
-        const speed = layer.getAttribute('data-speed');
-        const yPos = -scrollY * speed;
-        layer.style.transform = `translateY(${yPos}px)`;
-    });
+    // Adjust the translateY position based on scroll speed
+    foregroundLayer.style.transform = `translateY(-${scrollY * 0.5}px)`; // Adjust the scroll speed factor as needed
+    cliffLayer.style.transform = `translateY(-${scrollY * 1}px)`;
+    backgroundLayer.style.transform = `translateY(-${scrollY * 0.2}px)`; // Adjust the scroll speed factor as needed
 });
